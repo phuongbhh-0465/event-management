57e0443adab57c85f30a122ec2ceb191a0f1bb0f
phuongbhh-0465 <bach.huynh.hai.phuong@sun-asterisk.com>
2024-04-12 16:18:07 +0700

    attendees controller

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@ use App\Http\Controllers\Api\AttendeeController;
 use App\Http\Controllers\Api\EventController;
 
 Route::apiResource('events',EventController::class);
-Route::apiResource('events.attendees', AttendeeController::class)->scoped(['attendee' => 'event']);
+Route::apiResource('events.attendees', AttendeeController::class)->scoped()->except('update');
